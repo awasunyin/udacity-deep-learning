a23c048a44ee30f79a3881a528dd5779d3fa8a8b
awasunyin <awasunyin@gmail.com>
2017-12-02 11:50:46 +0100

    notes and exercises up to gradient descent implementation

---
+++ neural_networks/5_gradient_descent_functions.py
@@ -0,0 +1,26 @@
+# sigmoid
+# Activation (sigmoid) function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# output_formula
+# Output (prediction) formula
+def output_formula(features, weights, bias):
+    return sigmoid(np.dot(features, weights) + bias)
+
+# error_formula
+# Error (log-loss) formula
+def error_formula(y, output):
+    return - y*np.log(output) - (1 - y) * np.log(1-output)
+
+# update_weights
+# Gradient descent step
+def update_weights(x, y, weights, bias, learnrate):
+    output = output_formula(x, weights, bias)
+    d_error = -(y - output)
+    weights -= learnrate * d_error * x
+    bias -= learnrate * d_error
+    return weights, bias
+
+
+
